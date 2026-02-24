d2a1de5123bda868e726a2c95b7f40f5b9c3ec17
sier5213 <simonerikssson@gmail.com>
2026-02-24 14:26:54 +0100

    parse for chunks

---
+++ parsers.ts
@@ -0,0 +1,16 @@
+
+export function parse_args(): Record<string, string>  {
+    const args = process.argv.slice(2);
+    let args_record: Record<string, string> = {}
+    args.forEach((arg) => {
+        let [key, value]: string[] = arg.replace('--', '').split('=');
+        args_record[key] = value;
+    })
+    return args_record;
+}
+
+export function parse_content(header: string): string | null {
+    const match = header.match(/content-length:\s*(\d+)/i);
+    return match ? match[1] : null;
+}
+
