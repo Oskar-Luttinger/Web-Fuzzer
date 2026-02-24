ea059e8321aab0d525d3e1acf4ee4ada1b7763ff
Elis <wyver6@gmail.com>
2026-02-24 11:11:23 +0100

    removed js files

--- teast.ts
+++ teast.ts
@@ -1,5 +1,40 @@
 // Teast
-const str = 'HTTP/1.1 200 OK';
-const response_code = str.slice(8, 12);
-console.log(response_code);
-console.log('wtf');
+import { parse_args } from "./argparse";
+import { URL } from 'url';
+import net from "net";
+import * as fs from 'fs';
+
+const args = parse_args()
+const content: string = fs.readFileSync(args.path, 'utf-8');
+const url = new URL(args.url)
+
+async function TNR(url: URL, payload: string) {
+    const wsock = net.connect({host: url.hostname, port: Number(url.port)}, ()=>{})
+    let head_buff = ''
+    let body_buff = ''
+
+    wsock.on('connect', () => {
+        wsock.write(payload, 'utf-8')
+    })
+
+    function recv() {
+        return new Promise((resolve, reject) => {
+            try {
+                wsock.on('data', (chunk) => {
+                    head_buff += chunk
+                })
+                if (head_buff.length > 100) {
+                    resolve('recieved!')
+            } 
+            } catch (error) {
+                  console.log(error)
+                  reject(error)
+            } 
+        })
+    }
+    await recv()
+    console.log(head_buff)
+
+}
+
+TNR(url, content)
