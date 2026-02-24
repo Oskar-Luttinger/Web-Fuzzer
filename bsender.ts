9a7b0938646ac83f7130e5a0e4942a73078fdcff
Elis <wyver6@gmail.com>
2026-02-24 16:41:22 +0100

    Added status code parsing and improved the worker

--- bsender.ts
+++ bsender.ts
@@ -1,13 +1,9 @@
 // Bendy sender
 
 import net from "net";
-import tls from "tls";
 import * as fs from 'fs';
-import * as readline from "readline/promises"
-import {stdin as input, stdout as output } from "process";
-import { send_raw } from "./hello_web";
 import { URL } from 'url'; 
-import { parse_args, parse_content } from "./parsers"
+import { parse_args, parse_content, parse_status } from "./parsers"
 
 function inject(request : string, keyword: string, fuzzmarker?: string): string {
     if (!fuzzmarker) {
@@ -19,7 +15,7 @@ function inject(request : string, keyword: string, fuzzmarker?: string): string 
     return payload
 } 
 
-function snr(url: URL, payload: string) {
+function snr(url: URL, payload: string): Promise<string> {
     return new Promise((resolve, reject) => {
         try {
             let buffer = ''
@@ -29,9 +25,10 @@ function snr(url: URL, payload: string) {
             })
             wsock.on('data', function crec(chunk) {
                 buffer += chunk 
-                if (buffer.length > Number(parse_content(buffer)))
+                if (buffer.length > Number(parse_content(buffer))) {
                     wsock.off('data', crec)
                     resolve(buffer)
+                }
             })
             wsock.on('error', (error) => {
                 wsock.end()
@@ -45,25 +42,39 @@ function snr(url: URL, payload: string) {
 }
 
 
-let result_table: Array<Array<string|number>> = []
-
-async function worker(content, wlist) {
-    return new Promise((resolve, reject) => {
-        while (true)
-        if (wlist.is_empty) {
-            resolve('Done!')
-
-        }
+async function worker(content: string, wlist: Array<string>, url: URL) {
+    try {    
+        let result_table = []
+        while (wlist !== undefined && wlist.length > 0) {
+            let current_keyword = wlist.shift()
+            console.log(current_keyword)
+            if (current_keyword) {
+                let payload = inject(content, current_keyword)
+                let result = await snr(url, payload)
+                console.log(result)
+                let content_length = Number(parse_content(result))
+                let status_code = parse_status(result)
+                result_table.push([current_keyword, content_length, status_code])
+                }
+            }
+        return result_table
+        }  catch (error) {
+           console.log(error)
     }
 }
 
- // Get file path from user
- const args = parse_args()
- const content: string = fs.readFileSync(args.path, 'utf-8'); // Synchronous function, rest of program will wait until finished
- const url = new URL(args.url)
- const passwords: string = fs.readFileSync(args.wlist, 'utf-8'); 
- let wlist = passwords.split("\n").map(p => p.trim()).filter(p => p !== "");
- 
 
+// Get file path from user
+const args = parse_args()
+const content: string = fs.readFileSync(args.path, 'utf-8'); // Synchronous function, rest of program will wait until finished
+const url = new URL(args.url)
+const passwords: string = fs.readFileSync(args.wlist, 'utf-8'); 
+let wlist = passwords.split("\n").map(p => p.trim()).filter(p => p !== "");
 
+async function print_result() {
+    let result = await Promise.allSettled([worker(content, wlist, url)])
+    console.log(result)
+}
+
+print_result()
 
