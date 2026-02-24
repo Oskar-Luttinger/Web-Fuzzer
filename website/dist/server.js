eae7f2caef7f1ed94db00b5269e282aa3ec7969e
Oskar <grene.oskar.luttinger@gmail.com>
2026-02-24 15:01:47 +0100

    test2

--- website/dist/server.js
+++ website/dist/server.js
@@ -9,13 +9,13 @@ const app = (0, express_1.default)();
 const PORT = 3000;
 //Allows the server to understand the json files
 app.use(express_1.default.json());
-//Gives express acces to all of the website files
+//Gives express access to all of the website files
 app.use(express_1.default.static(path_1.default.join(__dirname, '../')));
-//Handels the data sent from the login form 
+//Handles the data sent from the login form 
 app.post('/login', (req, res) => {
     const { user, pass } = req.body;
     console.log(`Login attempt for user: ${user}`);
-    // Simple check (in a real app, you'd check a database here)
+    //Simple check (in a real app, you'd check a database here)
     if (user === 'admin' && pass === '1234') {
         res.status(200).json({ success: true, message: 'Succes, logged in' });
     }
@@ -23,8 +23,8 @@ app.post('/login', (req, res) => {
         res.status(401).json({ success: false, message: 'Invalid username or password.' });
     }
 });
-//Starts the servver
+//Starts the server
 app.listen(PORT, '127.0.0.1', () => {
     console.log(`Server is running!`);
-    console.log(`Access your site at: http://127.0.0.1:${PORT}/`);
+    console.log(`Access the site at: http://127.0.0.1:${PORT}/`);
 });
